4c9ec9582cfb74db42af6dd5a8face52576d8ebb
David Weir <dmw109@pitt.edu>
2022-02-18 15:36:23 -0500

    Added custom exceptions to client class.

--- pyrtma/client.py
+++ pyrtma/client.py
@@ -3,34 +3,83 @@ import select
 import time
 import os
 import ctypes
-from typing import List, Optional
 import pyrtma.internal_types
+
 from pyrtma.internal_types import Message
 from pyrtma.constants import *
+from functools import wraps
+from typing import List, Optional, Tuple
 
-DEBUG = False
+
+class ClientError(Exception):
+    """Base exception for all Client Errors."""
+
+    pass
 
 
-def debug_print(msg):
-    if DEBUG:
-        print(msg)
-    else:
-        pass
+class MessageManagerNotFound(ClientError):
+    """Raised when unable to connect to message manager."""
+
+    pass
+
+
+class NotConnectedError(ClientError):
+    """Raised when the client tries to read/write while not connected."""
+
+    pass
+
+
+class ConnectionLost(ClientError):
+    """Raised when there is a connection error with the server."""
+
+    pass
+
+
+class AcknowledgementTimeout(ClientError):
+    """Raised when client does not receive ack from message manager."""
+
+    pass
+
+
+class InvalidDestinationModule(ClientError):
+    """Raised when client tries to send to an invalid module."""
+
+    pass
+
+
+class InvalidDestinationHost(ClientError):
+    """Raised when client tries to send to an invalid host."""
+
+    pass
+
+
+def requires_connection(func):
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        if not self.connected:
+            raise NotConnectedError
+        else:
+            return func(self, *args, **kwargs)
+
+    return wrapper
 
 
 class Client(object):
     def __init__(self, module_id: int = 0, host_id: int = 0, timecode: bool = False):
-        self.module_id = module_id
-        self.host_id = host_id
-        self.msg_count = 0
-        self.start_time = time.time()
-        self.server = None
-        self.connected = False
-        self.header_cls = Message.get_header_cls(timecode)
+        self._module_id = module_id
+        self._host_id = host_id
+        self._msg_count = 0
+        self._server = ("", -1)
+        self._connected = False
+        self._header_cls = Message.get_header_cls(timecode)
 
     def __del__(self):
-        if self.connected:
-            self.disconnect()
+        if self._connected:
+            try:
+                self.disconnect()
+            except ClientError:
+                """Siliently ignore any errors at this point."""
+                pass
 
     def connect(
         self,
@@ -39,62 +88,74 @@ class Client(object):
         daemon_status: bool = False,
     ):
 
-        self.server = server_name.split(":")
-        self.server[1] = int(self.server[1])
-        self.server = tuple(self.server)
+        addr, port = server_name.split(":")
+        self._server = (addr, int(port))
 
-        self.type = "Socket"
+        # Create the tcp socket
+        self._sock = socket.socket(
+            family=socket.AF_INET, type=socket.SOCK_STREAM, proto=socket.IPPROTO_TCP
+        )
 
-        if self.type == "NamedPipe":
-            raise NotImplementedError
-        elif self.type == "Socket":
-            # Create the tcp socket
-            self.sock = socket.socket(
-                family=socket.AF_INET, type=socket.SOCK_STREAM, proto=socket.IPPROTO_TCP
-            )
+        # Connect to the message server
+        try:
+            self._connected = True
+            self._sock.connect(self._server)
+        except ConnectionRefusedError as e:
+            self._connected = False
+            raise MessageManagerNotFound(
+                "No message manager server responding at {self.ip_addr}:{self.port}"
+            ) from e
 
-            # Connect to the message server
-            self.sock.connect(self.server)
-            self.start_time = time.time()
+        # Disable Nagle Algorithm
+        self._sock.setsockopt(socket.getprotobyname("tcp"), socket.TCP_NODELAY, 1)
 
-            # Disable Nagle Algorithm
-            self.sock.setsockopt(socket.getprotobyname("tcp"), socket.TCP_NODELAY, 1)
+        self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-            self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        msg = pyrtma.internal_types.Connect()
+        msg.logger_status = int(logger_status)
+        msg.daemon_status = int(daemon_status)
 
-            msg = pyrtma.internal_types.Connect()
-            msg.logger_status = int(logger_status)
-            msg.daemon_status = int(daemon_status)
+        self.send_message(msg)
+        ack_msg = self.wait_for_acknowledgement()
 
-            self.send_message(msg)
-            ack_msg = self.wait_for_acknowledgement()
-            if ack_msg is None:
-                raise Exception("Failed to receive Acknowlegement from MessageManager")
-
-            # save own module ID from ACK if asked to be assigned dynamic ID
-            if self.module_id == 0:
-                self.module_id = ack_msg.header.dest_mod_id
-
-            self.connected = True
+        # save own module ID from ACK if asked to be assigned dynamic ID
+        if self._module_id == 0:
+            self._module_id = ack_msg.header.dest_mod_id
 
     def disconnect(self):
         try:
-            self.send_signal("Disconnect")
-            ack_msg = self.wait_for_acknowledgement(timeout=0.5)
-            if ack_msg is None:
-                raise Exception("Failed to receive Acknowlegement from MessageManager")
-            else:
-                self.clear_msg_buffer(timeout=0.5)
-        except BaseException as err:
-            if str(err) == "Failed to receive Acknowlegement from MessageManager":
-                raise err
+            if self._connected:
+                self.send_signal("Disconnect")
+                ack_msg = self.wait_for_acknowledgement(timeout=0.5)
         finally:
-            try:
-                self.sock.close()
-            except:
-                pass
-            self.connected = False
+            self._sock.close()
+            self._connected = False
 
+    @property
+    def server(self) -> Tuple[str, int]:
+        return self._server
+
+    @property
+    def ip_addr(self) -> str:
+        return self._server[0]
+
+    @property
+    def port(self) -> int:
+        return self._server[1]
+
+    @property
+    def connected(self) -> bool:
+        return self._connected
+
+    @property
+    def msg_count(self) -> int:
+        return self._msg_count
+
+    @property
+    def module_id(self) -> int:
+        return self._module_id
+
+    @requires_connection
     def send_module_ready(self):
         msg = pyrtma.internal_types.ModuleReady()
         msg.pid = os.getpid()
@@ -119,18 +180,23 @@ class Client(object):
             msg.msg_type = pyrtma.internal_types.MT[msg_name]
             self.send_message(msg)
 
+    @requires_connection
     def subscribe(self, msg_list: List[str]):
         self._subscription_control(msg_list, "Subscribe")
 
+    @requires_connection
     def unsubscribe(self, msg_list: List[str]):
         self._subscription_control(msg_list, "Unsubscribe")
 
+    @requires_connection
     def pause_subscription(self, msg_list: List[str]):
         self._subscription_control(msg_list, "PauseSubscription")
 
+    @requires_connection
     def resume_subscription(self, msg_list: List[str]):
         self._subscription_control(msg_list, "ResumeSubscription")
 
+    @requires_connection
     def send_signal(
         self,
         signal_name: str,
@@ -141,44 +207,40 @@ class Client(object):
 
         # Verify that the module & host ids are valid
         if dest_mod_id < 0 or dest_mod_id > MAX_MODULES:
-            raise Exception(
-                f"rtmaClient::send_message: Got invalid dest_mod_id [{dest_mod_id}]"
-            )
+            raise InvalidDestinationModule(f"Invalid dest_mod_id  of [{dest_mod_id}]")
 
         if dest_host_id < 0 or dest_host_id > MAX_HOSTS:
-            raise Exception(
-                f"rtmaClient::send_message: Got invalid dest_host_id [{dest_host_id}]"
-            )
+            raise InvalidDestinationHost(f"Invalid dest_host_id of [{dest_host_id}]")
 
         # Assume that msg_type, num_data_bytes, data - have been filled in
-        header = self.header_cls()
+        header = self._header_cls()
         header.msg_type = pyrtma.internal_types.MT[signal_name]
-        header.msg_count = self.msg_count
+        header.msg_count = self._msg_count
         header.send_time = time.time()
         header.recv_time = 0.0
-        header.src_host_id = self.host_id
-        header.src_mod_id = self.module_id
+        header.src_host_id = self._host_id
+        header.src_mod_id = self._module_id
         header.dest_host_id = dest_host_id
         header.dest_mod_id = dest_mod_id
         header.num_data_bytes = 0
 
         if timeout >= 0:
-            readfds, writefds, exceptfds = select.select([], [self.sock], [], timeout)
+            readfds, writefds, exceptfds = select.select([], [self._sock], [], timeout)
         else:
             readfds, writefds, exceptfds = select.select(
-                [], [self.sock], []
+                [], [self._sock], []
             )  # blocking
 
         if writefds:
-            self.sock.sendall(header)
+            self._sendall(header)
 
-            # debug_print(f"Sent {msg.msg_name}")
-            self.msg_count += 1
+            self._msg_count += 1
 
         else:
             # Socket was not ready to receive data. Drop the packet.
             print("x", end="")
 
+    @requires_connection
     def send_message(
         self,
         msg_data: ctypes.Structure,
@@ -188,61 +250,75 @@ class Client(object):
     ):
         # Verify that the module & host ids are valid
         if dest_mod_id < 0 or dest_mod_id > MAX_MODULES:
-            raise Exception(
-                f"rtmaClient::send_message: Got invalid dest_mod_id [{dest_mod_id}]"
-            )
+            raise InvalidDestinationModule(f"Invalid dest_mod_id of [{dest_mod_id}]")
 
         if dest_host_id < 0 or dest_host_id > MAX_HOSTS:
-            raise Exception(
-                f"rtmaClient::send_message: Got invalid dest_host_id [{dest_host_id}]"
-            )
+            raise InvalidDestinationHost(f"Invalid dest_host_id of [{dest_host_id}]")
 
         # Assume that msg_type, num_data_bytes, data - have been filled in
-        header = self.header_cls()
+        header = self._header_cls()
         header.msg_type = pyrtma.internal_types.MT[msg_data.__class__.__name__]
-        header.msg_count = self.msg_count
+        header.msg_count = self._msg_count
         header.send_time = time.time()
         header.recv_time = 0.0
-        header.src_host_id = self.host_id
-        header.src_mod_id = self.module_id
+        header.src_host_id = self._host_id
+        header.src_mod_id = self._module_id
         header.dest_host_id = dest_host_id
         header.dest_mod_id = dest_mod_id
         header.num_data_bytes = ctypes.sizeof(msg_data)
 
         if timeout >= 0:
-            readfds, writefds, exceptfds = select.select([], [self.sock], [], timeout)
+            readfds, writefds, exceptfds = select.select([], [self._sock], [], timeout)
         else:
             readfds, writefds, exceptfds = select.select(
-                [], [self.sock], []
+                [], [self._sock], []
             )  # blocking
 
         if writefds:
-            self.sock.sendall(header)
+            self._sendall(header)
             if header.num_data_bytes > 0:
-                self.sock.sendall(msg_data)
+                self._sendall(msg_data)
 
-            # debug_print(f"Sent {msg.msg_name}")
-            self.msg_count += 1
+            self._msg_count += 1
 
         else:
             # Socket was not ready to receive data. Drop the packet.
             print("x", end="")
 
+    def _sendall(self, buffer: bytes):
+        try:
+            self._sock.sendall(buffer)
+        except ConnectionError as e:
+            self._connected = False
+            raise ConnectionLost from e
+
+    @requires_connection
     def read_message(self, timeout=-1, ack=False) -> Optional[Message]:
         if timeout >= 0:
-            readfds, writefds, exceptfds = select.select([self.sock], [], [], timeout)
+            readfds, writefds, exceptfds = select.select([self._sock], [], [], timeout)
         else:
             readfds, writefds, exceptfds = select.select(
-                [self.sock], [], []
+                [self._sock], [], []
             )  # blocking
 
         # Read RTMA Header Section
         if readfds:
-            header = self.header_cls()
-            nbytes = self.sock.recv_into(header, header.size, socket.MSG_WAITALL)
-            assert (
-                nbytes == header.size
-            ), "Did not send all the header to message manager."
+            header = self._header_cls()
+            nbytes = self._sock.recv_into(header, header.size, socket.MSG_WAITALL)
+            """
+            Note:
+            MSG_WAITALL Flag:
+            The receive request will complete only when one of the following events occurs:
+
+            The buffer supplied by the caller is completely full.
+            The connection has been closed.
+            The request has been canceled or an error occurred.
+            """
+
+            if nbytes != header.size:
+                self._connected = False
+                raise ConnectionLost
+
             header.recv_time = time.time()
         else:
             return None
@@ -252,15 +328,18 @@ class Client(object):
 
         # Read Data Section
         if header.num_data_bytes:
-            nbytes = self.sock.recv_into(
+            nbytes = self._sock.recv_into(
                 msg.data_buffer, header.num_data_bytes, socket.MSG_WAITALL
             )
+
+            if nbytes != header.num_data_bytes:
+                self._connected = False
+                raise ConnectionLost
 
         return msg
 
     def wait_for_acknowledgement(self, timeout: float = 3):
         ret = 0
-        debug_print("Waiting for ACK... ")
 
         # Wait Forever
         if timeout == -1:
@@ -269,7 +348,6 @@ class Client(object):
                 if msg is not None:
                     if msg.header.msg_type == pyrtma.internal_types.MT["Acknowledge"]:
                         break
-                        debug_print("Got ACK!")
             return msg
         else:
             # Wait up to timeout seconds
@@ -279,18 +357,20 @@ class Client(object):
                 msg = self.read_message(timeout=time_remaining, ack=True)
                 if msg is not None:
                     if msg.header.msg_type == pyrtma.internal_types.MT["Acknowledge"]:
-                        debug_print("Got ACK!")
                         return msg
 
                 time_now = time.perf_counter()
                 time_waited = time_now - start_time
                 time_remaining = timeout - time_waited
 
-            debug_print("ACK timed out!")
-            return None
+            raise AcknowledgementTimeout(
+                "Failed to receive Acknowlegement from MessageManager"
+            )
 
-    def clear_msg_buffer(self, timeout: float = 1):
-        # read and discard all messages in buffer
+    def discard_messages(self, timeout: float = 1) -> bool:
+        """Read and discard messages in socket buffer up to timeout.
+            Returns: True if all messages available have been read. 
+        """
         msg = 1
         time_remaining = timeout
         start_time = time.perf_counter()
@@ -299,5 +379,8 @@ class Client(object):
             time_now = time.perf_counter()
             time_waited = time_now - start_time
             time_remaining = timeout - time_waited
-        if msg is not None:
-            debug_print("clear_msg_buffer timed out!")
+        return not msg
+
+    def __str__(self) -> str:
+        # TODO: Make this better.
+        return f"Client(server={self.server}, connected={self.connected}."
